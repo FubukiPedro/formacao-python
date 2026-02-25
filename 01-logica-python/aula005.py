e0349c139e8070e729bb761d161714b32b181385
Pedro Henrique <pedrohfubuki@gmail.com>
2026-02-24 21:38:53 -0300

    study: aula 5 - tipo de dados bool (boolean) - true ou false

--- 01-logica-python/aula005.py
+++ 01-logica-python/aula005.py
@@ -1,6 +1,6 @@
 # Tipo de dado bool (boolean)
-
-# Ao questionar algo em um programa, só existem duas respostas possíveis: sim (True) ou não (False).
+# Ao questionar algo em um programa, só existem duas respostas possíveis:
+#  sim (True) ou não (False).
 
 print(10 == 10)
 print(10 < 5)
