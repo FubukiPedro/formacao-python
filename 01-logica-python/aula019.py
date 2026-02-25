5d861b7ab5c51c02d741db979deb00b45acbaaa7
Pedro Henrique <pedrohfubuki@gmail.com>
2026-02-25 19:48:56 -0300

    study: aula 19 - operadores relacionais (de comparação) em python

---
+++ 01-logica-python/aula019.py
@@ -0,0 +1,18 @@
+"""
+Operadores de comparação (relacionais)
+OP      Significado         Exemplo (True)
+>       maior               2 > 1
+>=      maior ou igual      2 >= 2
+<       menor               1 < 2
+<=      menor ou igual      2 <= 2
+==      igual               'a' == 'a'
+!=      diferente           'a' != 'b'
+"""
+
+maior = 2 > 1
+maior_ou_igual = 5 > 3
+menor = 1 < 3
+menor_ou_igual = 1 <= 1 
+igual = 'a' == 'a'
+diferente = 'pedro' != 'fubuki'
+print(maior)
