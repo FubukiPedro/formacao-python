aa919f431d02fb92127b6446dcc258d9b9530936
Pedro Henrique <pedrohfubuki@gmail.com>
2026-02-24 21:34:30 -0300

    study: aula 1 - comentários e docstrings

--- 01-logica-python/aula001.py
+++ 01-logica-python/aula001.py
@@ -1,7 +1,7 @@
 # comentários em python
 print(123)
 # comentários utilizando "#" (comentário de verdade)
-print(456)
+print(4567)
 """isso é uma docString, não é um comentário"""
 '''a mesma coisa que a de cima, só que dessa vez usando ' '''
 print(789)
