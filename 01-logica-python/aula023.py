f3d7bd4200569e16c7064e625b0bd38879e599e4
Pedro Henrique <pedrohfubuki@gmail.com>
2026-02-26 14:24:25 -0300

    study: aula 23 - operador lógico "not"

---
+++ 01-logica-python/aula023.py
@@ -0,0 +1,16 @@
+# Operador lógico "not"
+
+# Usado para inverter expressões
+# not True = False
+# not False = True
+
+# o if só executa o bloco quando a condição é verdadeira (True).
+logado = False
+if not logado:
+    print('Você precisa fazer login.')
+# então quase se usa o "If not, o logado que é False, é executado com True."
+
+print()
+
+print(not True)  # False
+print(not False)  # True
