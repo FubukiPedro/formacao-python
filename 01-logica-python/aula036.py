50da1585067df696be76590f8af22d47661a4ec3
Pedro Henrique <pedrohfubuki@gmail.com>
2026-03-01 18:12:40 -0300

    study: aula 36 - operadores de atribuição com operadores aritméticos

---
+++ 01-logica-python/aula036.py
@@ -0,0 +1,16 @@
+"""
+Operadores de atribuição
+= += -= *= /= //= **= %=
+"""
+
+contador = 10
+
+# contador = contador * 2
+contador *= 2
+print(contador)
+
+contador //= 2
+print(contador)
+
+contador %= 3
+print(contador)
