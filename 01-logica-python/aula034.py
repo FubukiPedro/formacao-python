2272af74d5ac2ffcc95bde68ec3ea88a64f4d793
Pedro Henrique <pedrohfubuki@gmail.com>
2026-03-01 17:21:32 -0300

    study: aula 34 - while e break - estrutura de repetição (parte 1)

---
+++ 01-logica-python/aula034.py
@@ -0,0 +1,20 @@
+"""
+Repetições
+while (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+
+Loop infinito -> Quando um código não tem fim
+"""
+
+condicao = True
+
+while condicao:
+    nome = input('Digite o seu nome: ')
+    if nome == 'sair':
+        break
+    else:
+        print(f'Seu nome é {nome}')
+
+    
+
+print('O programa parou de executar.')
