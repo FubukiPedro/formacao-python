d0dc3ab20e002d284591fd34491fd67bd923bd86
Pedro Henrique <pedrohfubuki@gmail.com>
2026-03-04 15:44:54 -0300

    study: aula 41 - while / else

--- 01-logica-python/aula041.py
+++ 01-logica-python/aula041.py
@@ -1,1 +1,19 @@
 """ while/else """
+
+string = 'Valorqualquer'
+
+i = 0
+while i < len(string):
+    letra = string[i]
+    print(letra)
+
+    if letra == ' ':
+        print('Encontrei um espaço na string')
+        break
+
+    i += 1
+    
+else:
+    print('Não encontrei um espaço na string.')
+
+print('Fora do while.')
