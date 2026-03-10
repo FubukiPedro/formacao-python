4e5883f3c6e090603c80a637f5c6fa2be3c04c14
Pedro Henrique <pedrohfubuki@gmail.com>
2026-03-10 16:39:43 -0300

    study: aula 50 - exercício - exiba os índices da lista

--- 01-logica-python/aula050.py
+++ 01-logica-python/aula050.py
@@ -1,1 +1,26 @@
+"""
+Exercício
+Exiba os índices da lista
+0 Maria
+1 Helena
+2 Luiz
+"""
+
+# Jeito 1
+lista = ['Maria', 'Pedro', 'Pandinha', 'Piranha']
+lista.append('Henrique')
+indice = 0
+for nomes in lista:
+    print(f'{indice} {nomes}')
+    indice += 1
+
 print()
+
+# Jeito 2
+lista = ['Maria', 'Pedro', 'Pandinha', 'Piranha']
+lista.append('Henrique')
+
+indices = range(len(lista))
+
+for indice in indices:
+    print(indice, lista[indice], type(lista[indice]))
