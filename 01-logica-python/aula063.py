4b5208d7960ff7e645b79a14a3376552beb52393
Pedro Henrique <pedrohfubuki@gmail.com>
2026-03-17 17:24:34 -0300

    study: aula 63 - possíveis problemas e soluções para nosso algoritmo do CPF

---
+++ 01-logica-python/aula063.py
@@ -0,0 +1,42 @@
+import re 
+import sys
+
+"""cpf_enviado_usuario = '746.824.890-70' \
+    .replace('.', '') \
+    .replace('-', '') \
+    .replace(' ', '')"""
+entrada = input('CPF [746.824.890-70]: ')
+cpf_enviado_usuario = re.sub(
+    r'[^0-9]', '',
+entrada)
+
+entrada_sequencial = entrada == entrada[0] * len(entrada)
+
+if entrada_sequencial:
+    print('Voce enviou dados sequenciais')
+    sys.exit()
+
+nove_digitos = cpf_enviado_usuario [:9]
+resultado_digito_1 = 0
+contador_regressivo_1 = 10
+for digito in nove_digitos:
+    resultado_digito_1 += int(digito) * contador_regressivo_1
+    contador_regressivo_1 -= 1
+digito_1 = (resultado_digito_1 * 10) % 11
+digito_1 = digito_1 if digito_1 <= 9 else 0
+
+dez_digitos = nove_digitos + str(digito_1) 
+resultado_digito_2 = 0
+contagem_regressiva_2 = 11
+for digito in dez_digitos:
+    resultado_digito_2 += int(digito) * contagem_regressiva_2
+    contagem_regressiva_2 -= 1
+digito_2 = (resultado_digito_2 * 10) % 11
+digito_2 = digito_2 if digito_2 <= 9 else 0
+
+cpf_gerado_calculo = nove_digitos + str(digito_1) + str(digito_2)
+
+if cpf_gerado_calculo == cpf_enviado_usuario:
+    print(f'O CPF {cpf_enviado_usuario} é válido')
+else:
+    print(f'O CPF {cpf_enviado_usuario} é inválido')
