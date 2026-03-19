2224d31163d295ddca6ce0e186e6296a4cc45301
Pedro Henrique <pedrohfubuki@gmail.com>
2026-03-19 14:04:22 -0300

    study: aula 60 - operação ternária com python (if e else de uma linha)

--- 01-logica-python/aula060.py
+++ 01-logica-python/aula060.py
@@ -3,8 +3,8 @@ Operação ternária (condicional de uma linha)
 <valor> if <condicao> else <outro valor>
 """
 
-codicao = 10 == 11
-variavel = 'Valor' if codicao else 'Outro valor'
+condicao = 10 == 11
+variavel = 'Valor' if condicao else 'Outro valor'
 print(variavel)
 
 digito = 9 # > 9 = 0
