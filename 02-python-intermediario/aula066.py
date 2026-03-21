70550980363d90e605703a25f93b5d4e542f4150
Pedro Henrique <pedrohfubuki@gmail.com>
2026-03-21 15:43:40 -0300

    study: aula 66 - argumentos nomeados e argumentos posicionais (não nomeados) em funções

--- 02-python-intermediario/aula066.py
+++ 02-python-intermediario/aula066.py
@@ -1,3 +1,9 @@
+"""
+Argumentos nomeados e não nomeados em funções Python
+Argumento nomeado tem nome com sinal de igual
+Argumento não nomeado recebe apenas o argumento (valor)
+"""
+
 def soma(x, y, z):
     # Definição
     print(f'{x=} y={y} {z=}', '|', 'x + y =', x + y + z)
