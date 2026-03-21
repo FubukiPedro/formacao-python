d790b8b4a8c19bb19c7cbce58df2ff9bf5bcd039
Pedro Henrique <pedrohfubuki@gmail.com>
2026-03-21 15:32:08 -0300

    study: aula 66 - argumentos nomeados e argumentos posicionais (não nomeados) em funções

---
+++ 02-python-intermediario/aula066.py
@@ -0,0 +1,11 @@
+def soma(x, y, z):
+    # Definição
+    print(f'{x=} y={y} {z=}', '|', 'x + y =', x + y + z)
+
+soma(1, 2, 3)
+soma(2, 4, 5)
+soma(z=1, y=2, x=1)
+soma(z=1, x=2, y=1)
+soma(1, y=2, z=1)
+
+print(1, 2, 3, sep='-')
